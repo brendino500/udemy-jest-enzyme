8f238903bab232028ff858e52f1b5d41511a21f7
brendino500 <brenda.ty@live.com>
2020-11-07 19:43:57 +0000

    lecture 11 - shallow test, .debug() & expect()

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
-import Enzyme from "enzyme";
+import Enzyme, { shallow } from "enzyme";
 import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
 
 import App from "./App";
 
 Enzyme.configure({ adapter: new Adapter() });
 
-test("renders learn react link", () => {});
+test("renders without crashing", () => {
+  const wrapper = shallow(<App />); // shallow rendering
+  console.log(wrapper.debug()); // log debug as a String
+  expect(wrapper).toBeTruthy(); // throw an error is assertion is FALSE
+});
