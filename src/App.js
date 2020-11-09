3daa4fe88d749c2fd11083f7082ca95b0488a006
brendino500 <brenda.ty@live.com>
2020-11-09 13:28:58 +0000

    lecture 18 - setting up

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Learn React Testingq</h1>
-    </div>
-  );
+  return <div data-test=""></div>;
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,25 @@
 import Enzyme, { shallow } from "enzyme";
-import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
+import EnzymeAdapter from "@wojtekmaj/enzyme-adapter-react-17";
 
 import App from "./App";
 
-Enzyme.configure({ adapter: new Adapter() });
+// set up enzyme's react adapter
+Enzyme.configure({ adapter: new EnzymeAdapter() });
 
-test("renders without crashing", () => {
-  const wrapper = shallow(<App />); // shallow rendering
-  console.log(wrapper.debug()); // log debug as a String
-  expect(wrapper).toBeTruthy(); // throw an error is assertion is FALSE
+// test("renders without crashing", () => {
+//   const wrapper = shallow(<App />); // shallow rendering
+//   console.log(wrapper.debug()); // log debug as a String
+//   expect(wrapper).toBeTruthy(); // throw an error is assertion is FALSE
+// });
+
+test("renders without error", () => {
+  const wrapper = shallow(<App />);
 });
+
+test("renders button", () => {});
+
+test("renders counter display", () => {});
+
+test("counter starts at 0", () => {});
+
+test("clicking on button increments counter display", () => {});
