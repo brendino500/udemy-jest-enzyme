fcfe0b5a56550037f0a7d0b56bb8fe0661fa4fb1
brendino500 <brenda.ty@live.com>
2020-11-09 14:09:03 +0000

    lecture 20 - disabling test attribute on render

--- src/App.js
+++ src/App.js
@@ -2,11 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return (
-    <div data-test="component-app">
-      <h1>APP</h1>
-    </div>
-  );
+  return <div data-test="component-app"></div>;
 }
 
 export default App;
