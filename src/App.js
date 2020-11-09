a76b8cf00dc154a248480e3c1dfd4834924299c3
brendino500 <brenda.ty@live.com>
2020-11-09 14:00:13 +0000

    test

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div data-test="component-app"></div>;
+  return (
+    <div data-test="component-app">
+      <h1>APP</h1>
+    </div>
+  );
 }
 
 export default App;
