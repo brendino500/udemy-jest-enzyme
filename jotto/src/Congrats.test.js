c3d063930378ed5774bd5ca8a9f05d6e3a931468
brendino500 <brenda.ty@live.com>
2020-11-10 14:37:39 +0000

    lecture 36 - prop types testing

--- jotto/src/Congrats.test.js
+++ jotto/src/Congrats.test.js
@@ -3,7 +3,7 @@ import EnzymeAdapter from "@wojtekmaj/enzyme-adapter-react-17";
 import React from "react";
 
 import Congrats from "./Congrats";
-import { findByTestAttr } from "../test/testUtils";
+import { findByTestAttr, checkProps } from "../test/testUtils";
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
@@ -34,4 +34,9 @@ test("renders non-empty congrats message when `success` prop is true", () => {
   const wrapper = setup({ success: true });
   const message = findByTestAttr(wrapper, "congrats-message");
   expect(message.text().length).not.toBe(0);
+});
+
+test("does not throw warning with epect props", () => {
+  const expectedProps = { success: false };
+  checkProps(Congrats, expectedProps);
 });
