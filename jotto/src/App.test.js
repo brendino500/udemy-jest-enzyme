df48978ed8cfd885f4f64eaff223ea8a03f9196c
brendino500 <brenda.ty@live.com>
2020-11-10 13:57:07 +0000

    set up

---
+++ jotto/src/App.test.js
@@ -0,0 +1,13 @@
+import { render, screen } from "@testing-library/react";
+import EnzymeAdapter from "@wojtekmaj/enzyme-adapter-react-17";
+import React from "react";
+
+import App from "./App";
+
+EnzymeAdapter.configure({ adapter: new EnzymeAdapter() });
+
+test("renders learn react link", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
