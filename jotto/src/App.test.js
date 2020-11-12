b50c59b7ec45925cc95a1baa4b734b375f7b7f9f
brendino500 <brenda.ty@live.com>
2020-11-12 17:41:39 +0000

    lectuer 117 - test getSecretWord is called on AppMount

--- jotto/src/App.test.js
+++ jotto/src/App.test.js
@@ -1,22 +1,36 @@
 import { render, screen } from "@testing-library/react";
 // import EnzymeAdapter from "@wojtekmaj/enzyme-adapter-react-17";
 import React from "react";
-import { shallow } from "enzyme";
+import { mount } from "enzyme";
 import { findByTestAttr } from "../test/testUtils";
-
 import App from "./App";
+import hookActions from "./actions/hookActions";
 
-// EnzymeAdapter.configure({ adapter: new EnzymeAdapter() });
+const mockGetSecretWord = jest.fn();
+
 /**
  * Setup function for app component
- * @returns {ShallowWrapper}
+ * @returns {ReactWrapper}
  */
 const setup = () => {
-  return shallow(<App />);
+  mockGetSecretWord.mockClear();
+  hookActions.getSecretWord = mockGetSecretWord;
+
+  // use mount because useEffect isn't called on 'shallow'
+  return mount(<App />);
 };
 
 test("App renders without error", () => {
   const wrapper = setup();
   const component = findByTestAttr(wrapper, "component-app");
   expect(component.length).toBe(1);
+});
+
+describe("getSecretWord calls", () => {
+  test("getSecretWord gets called on App mount", () => {
+    setup();
+
+    // check to see if secret word was updated
+    expect(mockGetSecretWord).toHaveBeenCalled();
+  });
 });
