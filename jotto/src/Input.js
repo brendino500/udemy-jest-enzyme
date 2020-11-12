9ab227ab0fe5ecab2be51ce9b9f2640da270400f
brendino500 <brenda.ty@live.com>
2020-11-12 14:59:34 +0000

    lecture 110 - prepare input for writing setState test

--- jotto/src/Input.js
+++ jotto/src/Input.js
@@ -2,10 +2,27 @@ import React from "react";
 import PropTypes from "prop-types";
 
 function Input({ secretWord }) {
-  return <div data-test="component-input"></div>;
+  const [currentGuess, setCurrentState] = React.useState("");
+
+  return (
+    <div data-test="component-input">
+      <form className="form-inline">
+        <input
+          data-test="input-box"
+          className="mb-2 mx-sm-3"
+          type="text"
+          placeholder="enter guess"
+        />
+        <button data-test="submit-button" className="btn btn-primary mb-2">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
 
 Input.propTypes = {
   secretWord: PropTypes.string.isRequired,
 };
+
 export default Input;
