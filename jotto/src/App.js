c2fd8b1d9071df26c39026849a7dfe959466d0d3
brendino500 <brenda.ty@live.com>
2020-11-12 14:32:53 +0000

    lecture 106 - basic app component

--- jotto/src/App.js
+++ jotto/src/App.js
@@ -1,20 +1,9 @@
 import "./App.css";
 import React, { Component } from "react";
 
-import GuessedWords from "./GuessedWords";
-import Congrats from "./Congrats";
-
 class App extends Component {
   render() {
-    return (
-      <div className="container">
-        <h1>Jotto</h1>
-        <Congrats success={true} />
-        <GuessedWords
-          guessedWords={[{ guessedWord: "train", letterMatchCount: 3 }]}
-        />
-      </div>
-    );
+    return <div data-test="component-app"></div>;
   }
 }
 
--- jotto/src/App.test.js
+++ jotto/src/App.test.js
@@ -1,13 +1,22 @@
 import { render, screen } from "@testing-library/react";
 // import EnzymeAdapter from "@wojtekmaj/enzyme-adapter-react-17";
 import React from "react";
+import { shallow } from "enzyme";
+import { findByTestAtt, findByTestAttr } from "../test/testUtils";
 
 import App from "./App";
 
 // EnzymeAdapter.configure({ adapter: new EnzymeAdapter() });
+/**
+ * Setup function for app component
+ * @returns {ShallowWrapper}
+ */
+const setup = () => {
+  return shallow(<App />);
+};
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("App renders without error", () => {
+  const wrapper = setup();
+  const component = findByTestAttr(wrapper, "component-app");
+  expect(component.length).toBe(1);
 });
