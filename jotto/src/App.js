b2bbf2bd34566e321a85097a200ba0c0ac01f0fd
brendino500 <brenda.ty@live.com>
2020-11-13 11:14:42 +0000

    lectuer 119 - useEffect() & useReducer()

--- jotto/src/App.js
+++ jotto/src/App.js
@@ -1,10 +1,34 @@
 import "./App.css";
-import React, { Component } from "react";
+import React from "react";
+import hookActions from "./actions/hookActions";
 
-class App extends Component {
-  render() {
-    return <div data-test="component-app"></div>;
+/**
+ * reducer to update state, called automatically by dispatch
+ * @param state {object} - existing state
+ * @param action {object}- contains 'type' and 'payload' properties for the state update
+ * for example: { type: "setSecretWord", payload: "party" }
+ * @return {object} - new state
+ */
+function reducer(state, action) {
+  switch (action.type) {
+    case "setSecretWord":
+      return { ...state, secretWord: action.payload };
+    default:
+      throw new Error(`invalid action type: ${action.type}`);
   }
+}
+
+function App() {
+  const [state, dispatch] = React.useReducer(reducer, { secretWord: null });
+
+  const setSecretWord = (secretWord) =>
+    dispatch({ type: "setSecretWord", payload: secretWord });
+
+  React.useEffect(() => {
+    hookActions.getSecretWord(setSecretWord);
+  }, []);
+
+  return <div data-test="component-app"></div>;
 }
 
 export default App;
